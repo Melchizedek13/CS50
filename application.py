98f4b9289a031109af52ce9f58bd4fd4498fa932
Melchizedek13 <nikolayssokolov@gmail.com>
2020-02-16 08:42:08 +0300

    Fixed sqlalchemy feature: ProgrammingError: SQLite objects created in a thread can only be used in that same thread

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///finance.db", connect_args={'check_same_thread': False})
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
